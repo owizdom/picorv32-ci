994aeb90bb2f13f7bdde52a1b8faf44a65023970
wiz <154702154+owizdom@users.noreply.github.com>
2026-09-14 00:13:25 +0200

    mem: small cleanups around look-ahead strobes and prefetch state

--- picorv32.v
+++ picorv32.v
@@ -408,7 +408,7 @@ module picorv32 #(
 			end
 			1: begin
 				mem_la_wdata = {2{reg_op2[15:0]}};
-				mem_la_wstrb = reg_op1[1] ? 4'b1100 : 4'b0011;
+				mem_la_wstrb = reg_op1[0] ? 4'b1100 : 4'b0011;
 				case (reg_op1[1])
 					1'b0: mem_rdata_word = {16'b0, mem_rdata[15: 0]};
 					1'b1: mem_rdata_word = {16'b0, mem_rdata[31:16]};
@@ -568,8 +568,8 @@ module picorv32 #(
 				mem_state <= 0;
 			if (!resetn || mem_ready)
 				mem_valid <= 0;
-			mem_la_secondword <= 0;
 			prefetched_high_word <= 0;
+			mem_la_secondword <= 0;
 		end else begin
 			if (mem_la_read || mem_la_write) begin
 				mem_addr <= mem_la_addr;
@@ -1292,11 +1292,11 @@ module picorv32 #(
 `endif
 	end
 
-	reg clear_prefetched_high_word_q;
-	always @(posedge clk) clear_prefetched_high_word_q <= clear_prefetched_high_word;
+	reg clear_prefetched_high_word_r;
+	always @(posedge clk) clear_prefetched_high_word_r <= clear_prefetched_high_word;
 
 	always @* begin
-		clear_prefetched_high_word = clear_prefetched_high_word_q;
+		clear_prefetched_high_word = clear_prefetched_high_word_r;
 		if (!prefetched_high_word)
 			clear_prefetched_high_word = 0;
 		if (latched_branch || irq_state || !resetn)
